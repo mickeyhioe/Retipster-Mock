0ad7e7993a0bf5ce87d0c9e7f293d9b7189b36fc
Mickey Hioe <info@mickeyhioe.com>
2018-09-25 18:30:56 -0400

    CSS Update - Title Icon

---
+++ about.php
@@ -0,0 +1,135 @@
+<?php
+  $page = "About";
+  include('includes/header.php');
+?>
+
+    <!-- Section Intro -->
+    <section id="section-intro" class="section-padding">      
+
+      <div class="grid-container">
+        <div class="grid-x grid-padding-x align-center">
+
+          <div class="large-6 medium-10 cell">
+
+            <!-- Video -->
+            <div class="video-container-wave">
+              <div class="responsive-embed widescreen">
+                <iframe width="500" height="281" src="https://www.youtube.com/embed/m4lQSw6ADmw?feature=oembed" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            <!-- /Video -->
+
+          </div>
+
+
+          <div class="large-6 medium-10 cell">
+             <!-- Main -->
+             <div>
+              <h1 class="text-blue">If you’ve ever asked yourself these questions, you’re in the right place</h1>
+              <ul>
+                <li>How is it that some people make millions from real estate, but I can’t seem to get a single deal done? What am I missing??</li>
+                <li>How can I make great money from real estate WITHOUT risking everything I have? Is that even possible?</li>
+                <li>Where do I find those “highly motivated sellers” who will sell their real estate for practically nothing? I want to build SEVERAL streams of passive income… but how can I do it without waiting 30+ years?</li>
+                <li>I really want to quit my job. How can I do it with real estate?</li>
+                <li>I need to get this property sold TODAY. How do I make this happen FAST?</li>
+                <li>I want to invest in real estate – but how on earth do I get started??</li>
+              </ul>
+            </div>
+            <!-- /Main -->
+        
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    <!-- /Section Intro -->
+
+    <!-- Section Guidance -->
+    <section id="guidance" class="">
+      <div class="grid-container align-center">
+        <div class="grid-x text-center">
+          <div class="cell">
+            <h2 class="title-heart text-blue">Real world guidance for real estate investing</h2>
+            <p><em>There are those who complicate simplicity, and those who simplify complexities.</em></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section Guidance -->
+
+    <!-- Section Discover Freedom -->
+    <section id="discover-freedom" class="section-padding">
+      <div class="grid-container align-center">
+        <div class="grid-x grid-padding-x">
+          <div class="large-6 medium-10 cell">
+            <img src="assets/img/img-couple.svg" alt="Couple">
+          </div>
+          <div class="large-6 medium-10 cell">
+            <h2 class="text-blue">Discover freedom</h2>
+            <p>Making money while minimizing wasted time and wasted effort is not an impossible riddle. Gathered here are the tools you need for success in high-return, low-risk real estate investment for personal and financial freedom.</p>
+            <p>We’ve taken everything we’ve learned in successful real estate investing, and turned them into easy-to-understand, simple-to-follow, and proven-to-work tips. All you need to do is to learn and apply.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section Discover Freedom -->                  
+
+    <?php include('includes/section-subscribe.php'); ?>
+
+    <!-- Section Menifesto -->
+    <section id="manifesto" class="section-padding">
+      <div class="grid-container align-center">
+        <div class="grid-x grid-padding-x">
+          <div class="large-6 medium-10 cell">
+            <img src="assets/img/img-working.svg" alt="Working">
+          </div>
+          <div class="large-6 medium-10 cell">
+            <h2 class="text-blue">Menifesto</h2>
+            <p><strong>We believe in education and empowerment</strong><br>Methods that are understandable, actionable, and accessible make the best lessons.</p>
+            <p><strong>We believe that real success is gimmick-free</strong><br>You don’t need to open your wallet to get it, but you do need to open your mind and take action.</p>
+            <p><strong>We believe you can start and grow a great business</strong><br>You don’t need to open your wallet to get it, but you do need to open your mind and take action.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section Menifesto -->
+
+    <!-- Section Meet Seth -->
+    <section id="guidance" class="">
+      <div class="grid-container align-center">
+        <div class="grid-x text-center">
+          <div class="cell">
+            <h2 class="title-hand text-blue">Meet Seth Williams</h2>
+            <p><em>There are those who complicate simplicity, and those who simplify complexities.</em></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section Section Meet Seth -->
+
+    <!-- Section My Journey -->
+    <section id="my-journey" class="section-padding">
+      <div class="grid-container align-center">
+        <div class="grid-x grid-padding-x">
+          <div class="large-6 medium-10 cell">
+            <img src="assets/img/img-working.svg" alt="Working">
+          </div>
+          <div class="large-6 medium-10 cell">
+            <h2 class="text-blue">My journey</h2>
+            <p>Hi, I’m Seth – founder of REtipster. The lessons I learned on my journey to real estate investing success gave me a new mission: to empower others to their own create personal and financial freedom.</p>
+            <p>On this journey I didn’t stumble upon any secret shortcuts — I learned through <strong>trial and error</strong>. Now I’m sharing my knowledge and experience to make the internet a better place for real estate investors.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section My Journey -->
+
+
+
+    <?php include('includes/section-social.php'); ?>
+
+  </div>
+
+    
+<?php include('includes/footer.php');?>
